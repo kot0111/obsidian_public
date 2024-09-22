aa8d5f2b27d36137dd6dcc9b99a1a3a3c5dfe53d
kot0111 <kozyura0111@gmail.com>
2024-09-22 13:28:10 +0300

    Quartz sync: Sep 22, 2024, 1:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Kot0111",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kot0111.github.io/obsidian_public",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
